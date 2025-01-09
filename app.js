6a39d527f75c1373005246a3261f1649fb7d2919
Daniel Martinez <edu211004@gmail.com>
2025-01-09 13:00:15 -0600

    connection

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const authRouter = require('./controllers/auth')
+const { default: mongoose } = require('mongoose')
+
+const CONNECTION_STRING = 'mongodb://localhost:27017'
 
 const app = express()
+
+mongoose.connect(CONNECTION_STRING)
+.then(()=> console.log('Joya'))
+.catch(err=> console.log(err))
 
 app.use(authRouter)
 
