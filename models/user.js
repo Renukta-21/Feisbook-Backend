b5a0205a3b940392d89d1f890f59aea81e684f50
Daniel Martinez <edu211004@gmail.com>
2025-01-09 13:50:51 -0600

    expanded Schema

--- models/user.js
+++ models/user.js
@@ -1,11 +1,33 @@
-const { default: mongoose, model } = require("mongoose");
+const { default: mongoose, model, Mongoose } = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
         trim:true
-    }
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    passwordHash:{
+        required:true,
+        type:String
+    },
+    profilePicture:{
+        type:String,
+    },
+    coverPhoto:{
+        type:String
+    },
+    bio:{
+        type:String
+    },
+    friends:[{
+        type: mongoose.Types.ObjectId,
+        ref:'User'
+    }]
 })
 
 const User = mongoose.model('User', userSchema)
