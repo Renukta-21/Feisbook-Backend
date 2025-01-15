f65aea6f91ce06f5773102b3c69a4227681c1001
Daniel Martinez <edu211004@gmail.com>
2025-01-15 13:38:51 -0600

    more tests

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const User = require('../models/user');
 
 const api = supertest(app)
+const apiURL = '/api/auth/signup'
 
 beforeEach(async () => {
     await User.deleteMany({})
@@ -33,21 +34,38 @@ const validationTests = [
 describe('Sign up', () => {
     test('Fails if property is missing', async () => {
         for(const {data, missingField, expectedMessage} of validationTests){
-            const response = await api.post('/api/auth/signup')
+            const response = await api.post(apiURL)
             .send(data)
             .expect(400)
-            console.log(response.body)
             assert.ok(response.body.error.includes(`User validation failed: ${missingField}: Path \`${missingField}\` is required.`))
         }
     })
     test('Password does not meet the minimun length or missing', async()=>{
-        const response = await api.post('/api/auth/signup')
+        const response = await api.post(apiURL)
         .send({
             "name":"Anibru Martinez",
             "email":"eduz211004@gmail.com",
             "password": "Daniel"
         })
         .expect(400)
+    })
+    test('Email already exists', async()=>{
+        await api.post(apiURL)
+        .send({
+            "name":"Eduardo Urbina",
+            "email": "eduz211004@gmail.com",
+            "password": "Daniel211004",
+            "bio": "Never expect something better, better things always brings sadder moods"
+        })
+        const response = await api.post(apiURL)
+        .send({
+            "name":"Daniel Urbina",
+            "email": "eduz211004@gmail.com",
+            "password": "Daniel211004",
+            "bio": "Never expect something better, better things always brings sadder moods"
+        })
+        .expect(409)
+        assert.ok(response.body.error.includes('has an existing account'))
     })
 })
 
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,13 +1,16 @@
 const errorHandler = (err, req, res, next)=>{
-    console.log('Error --------------------------------')
+    if(process.env.NODE_ENV !== 'test'){
+        console.log('Error --------------------------------')
+    }
     if(err.code === 11000){
         const [[, value]] = Object.entries(err.keyValue)
         res.status(409).send({error:`${value} has an existing account`})
     }else if(err.name === 'ValidationError') {
-        console.log(err.message)
         res.status(400).send({error:err.message})
     }else{
-        console.log('Nuevo errror xdd '+ err.name)
+        if(process.env.NODE_ENV !== 'test'){
+            console.log('Nuevo errror xdd '+ err.name)
+        }
     }
 }
 
