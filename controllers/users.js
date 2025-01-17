d0c50e652d66d3c1ab688a27933a296d9e49bba6
Daniel Martinez <edu211004@gmail.com>
2025-01-17 09:23:59 -0600

    sending token

--- controllers/users.js
+++ controllers/users.js
@@ -5,6 +5,8 @@ usersRouter.get('/me', async(req,res)=>{
     const {email} = req.body
     const user = await User.findOne({email})
     
+    if(!user) res.status(404).send({error:'User not found'})
+
     res.status(200).send({user})
 })
 usersRouter.get('/:id', async(req,res)=>{
