e6d7d78065a1035ae1de0a1d6c4e2f06c5593474
Daniel Martinez <edu211004@gmail.com>
2025-01-22 13:10:56 -0600

    Remove .env from repository

--- config.js
+++ config.js
@@ -2,6 +2,9 @@ require('dotenv').config()
 
 const config = {
     mongoURI : process.env.MONGO_URI_TEST,
+    cloud_name : process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret :process.env.API_SECRET,
     port : 3005
 }
 
--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,1 +1,10 @@
+const config = require('../config');
 const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({ 
+    cloud_name: config.cloud_name, 
+    api_key: config.api_key, 
+    api_secret: config.api_secret,
+    secure:true
+});
+export const uploadImage = ()=>{}
